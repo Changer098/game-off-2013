2dbd050ea1f85b46506928afaf59830a1548364c
Ryan Everett <Ryanrocks2008@hotmail.com>
2013-11-30 17:45:57 -0500

    Unity Upload

---
+++ Assets/Scripts/bullet.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class bullet : MonoBehaviour {
+	public float wake;
+	public float dead;
+	public float time;
+	// Use this for initialization
+	void Start()
+	{
+		this.rigidbody.AddForce (Vector3.back*10000);
+		wake=Time.fixedDeltaTime;
+		dead=wake+5;
+		StartCoroutine ("Kill");
+	}
+	void OnCollisionEnter(Collision obj)
+	{
+		if(obj.gameObject.tag=="boulder")
+		{
+			Destroy (gameObject);
+		}
+		else
+		{
+			Destroy (gameObject);
+		}
+	}
+	IEnumerator Kill()
+	{
+
+		yield return new WaitForSeconds(1);
+		Destroy (gameObject);
+	}
+}
---
+++ Assets/Scripts/score.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class score : MonoBehaviour {
+	public static int finalscore;
+	public static string finaltime;
+	public int time;
+	public int kills;
+	public int coins;
+	float time1;
+	// Use this for initialization
+	void Awake () {
+		finalscore=0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time1 = Time.time;
+		time = Mathf.FloorToInt(time1);
+		kills = Mathf.FloorToInt(Player_ship.kills);
+		coins = Mathf.FloorToInt(Player_ship.coins);
+		finalscore = time + kills + coins*100;
+		if(Player_ship.isDead==true)
+		{
+			CalculateTime ();
+		}
+	}
+	void CalculateTime()
+	{
+		if(time<60)
+		{
+			finaltime=time.ToString () + " seconds";
+		}
+		else
+		{
+			int min;
+			float min1;
+			int seconds;
+			min1=time/60;
+			min=Mathf.FloorToInt (min1);
+			seconds=time % 60;
+
+			string mint = min.ToString();
+			string sect = seconds.ToString();
+			finaltime = mint + ":" + sect;
+		}
+	}
+}
